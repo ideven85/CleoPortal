778c19c3412af6b6f1023e0df041499e3f84467f
Deven Kalra <deven@Macbook.local>
2023-09-18 15:58:02 +0530

    Seed

--- core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
-
-# Create your views here.
-def index(request):
-    return render(request,'home.html')
