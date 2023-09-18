be9d648900d26bb4ba82eb91a858cf8e61c1b6e8
Deven Kalra <deven@Macbook.local>
2023-09-18 15:59:34 +0530

    Seed

---
+++ src/test/java/com/cleo/IPL_App/IplAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleo.IPL_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
